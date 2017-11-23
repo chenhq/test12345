582efb50f5f21cc471dc67eaa9be4671d961bbc5
chenhq <hongqing.chen@gmail.com>
2017-11-23 22:56:51 +0800

    x

--- physics_model.py
+++ physics_model.py
@@ -1,18 +1,19 @@
 import numpy as np
 
 
-def angle_degree(x, y):
+def angle_size(x, y):
     return np.arctan2(x, y)
 
 
-def acceleration(g, alpha, drag_ratio):
-    a = g * np.sin(alpha) - g * np.cos(alpha) * drag_ratio
+def acceleration(alpha, drag_ratio):
+    a = np.sin(alpha) - np.cos(alpha) * drag_ratio
     return a
 
 
 def velocity(v0, a, s):
     vt_square = np.square(v0) + 2 * a * s
-    vt = np.sqrt(vt_square)
     if vt_square < 0:
-        vt = -vt
+        vt = 0
+    else:
+        vt = np.sqrt(vt_square)
     return vt
